3d40c4981bdbe0838d590ade8392f7e3d73af1ba
Cloud_Jumper <Alpha_Centauri_reso@proton.me>
2025-05-14 02:49:06 -0400

    First Code Push!

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ThePostProcessor")]
+[assembly: AssemblyProduct("ThePostProcessor")]
+[assembly: AssemblyDescription("Description of your mod")]
+[assembly: AssemblyCopyright("Copyright © 2024 ExampleAuthor")]
+[assembly: AssemblyVersion(ThePostProcessor.ThePostProcessor.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(ThePostProcessor.ThePostProcessor.VERSION_CONSTANT)]
